aeecf285fc4326ae5fd3723b8c3400fdde3d6738
Khaliqyar Haidary <khaliqyar18@gmail.com>
2021-03-06 11:55:13 -0500

    This is my first Dav Project

---
+++ src/test/java/com/collection/Main.java
@@ -0,0 +1,18 @@
+package com.collection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        // write your code here
+
+        for(int i =0; i<5;i++){
+            System.out.println("Hello Batch 8");
+            System.out.println(" I aam commited to learn this skill");
+            System.out.println("Hello world");
+
+            System.out.println(" This is new changes ");
+            Thread.sleep(2000);
+
+        }
+    }
+}
